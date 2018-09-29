c3a13ae8d42e39933b6bd1b8a49a45b24627538d
huangzezhou <764804602@qq.com>
2018-09-29 16:28:53 +0800

    删除多余注释

--- src/test/java/com/justplay1994/github/db2es/ThreadTest.java
+++ src/test/java/com/justplay1994/github/db2es/ThreadTest.java
@@ -1,16 +1,4 @@
 package com.justplay1994.github.db2es;
-/**********************************************************************
- * Copyright (c) 2018 CETC Company
- * 中电科新型智慧城市研究院有限公司版权所有
- * <p>
- * PROPRIETARY RIGHTS of CETC Company are involved in the
- * subject matter of this material. All manufacturing, reproduction, use,
- * and sales rights pertaining to this subject matter are governed by the
- * license agreement. The recipient of this software implicitly accepts
- * the terms of the license.
- * 本软件文档资料是中电科新型智慧城市研究院有限公司的资产，任何人士阅读和
- * 使用本资料必须获得相应的书面授权，承担保密责任和接受相应的法律约束。
- *************************************************************************/
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
